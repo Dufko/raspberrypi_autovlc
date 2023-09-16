eb00679d0ff41cbcdbb0153a28ad81644bfc9238
Michal Dufek <dufekmi2@fit.cvut.cz>
2023-09-16 21:12:32 +0200

    Add comment

--- start_vlc.sh
+++ start_vlc.sh
@@ -1,2 +1,2 @@
-sudo mount -o ro /dev/sda /media/flashdisk/
-cvlc "/media/flashdisk/"  --playlist-autostart --loop
+sudo mount -o ro /dev/sda /media/flashdisk/ # or use lsblk to find path to correct device and change here
+cvlc "/media/flashdisk/"  --playlist-autostart --loop --no-video-title-show 
